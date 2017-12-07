d123c5fa7ff6e5826edee0af10fc0bc5d6503516
kristinadrashkova <kristinadrashkova@gmail.com>
2017-12-07 10:28:15 +0200

    Added Context class

---
+++ src/main/java/com/musala/generala/strategy/Context.java
@@ -0,0 +1,15 @@
+package com.musala.generala.strategy;
+
+import com.musala.generala.models.Employee;
+
+public class Context {
+    private IStrategy strategy;
+
+    public Context(IStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public <T>T executeStrategy(Employee... employees) {
+        return strategy.doOperation(employees);
+    }
+}
